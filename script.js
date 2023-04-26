47c8d1dc24637d26159bb436186c4cf7f7e80ef4
JericFederer <pianocoderodyssey@gmail.com>
2023-04-26 15:16:40 +0900

    updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
+const pathName = document.location.pathname;
 
 let ready = false;
 let imagesLoaded = 0;
@@ -9,7 +10,17 @@ let photosArray = [];
 // Unsplash API
 let count = 5;
 const apiKey = 'UA9wuwnEHNRPRGD_CahW5lgUcGAbNseLvGgbgMgmJdY';
-let apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=japan autumn`;
+var apiURL;
+
+if (pathName.includes("piano")) {
+  apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=piano`;
+} else if (pathName.includes("nightsky")) {
+  apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=nightsky`;
+} else if (pathName.includes("universe")) {
+  apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=universe`;
+} else if (pathName.includes("ghibli")) {
+  apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=aurora`;
+}
 
 // Check if all images were loaded
 function imageLoaded() {
